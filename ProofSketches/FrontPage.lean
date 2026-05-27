073bbd9913edcab6361f9bee63d22c7580bdc181
George McNinch <gmcninch@gmail.com>
2026-05-26 22:27:52 -0400

    update on 2026-05-26 22:27:51 by george@valhalla

--- ProofSketches/FrontPage.lean
+++ ProofSketches/FrontPage.lean
@@ -5,5 +5,5 @@ open Verso Genre Blog
 #doc (Page) "Proof Sketches" =>
 
 This page contains discussion of formalization of mathematics via Lean.
-[See here](/posts/) for the posts.
+[See here](/posts) for the posts.
 
