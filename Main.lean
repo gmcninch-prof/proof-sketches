28cc9e729ae30573f9de025d8d8a8fbb7b2221c3
George McNinch <gmcninch@gmail.com>
2026-05-26 21:52:07 -0400

    update on 2026-05-26 21:52:04 by george@valhalla

--- Main.lean
+++ Main.lean
@@ -7,26 +7,19 @@ import ProofSketches.Posts.REU
 open Verso Genre Blog Site Syntax 
 
 def linkTargets : Code.LinkTargets TraverseContext where
-  const n _      := #[{ shortDescription := "doc"
-                       , description := s!"Mathlib docs for {n}"
-                       , href := s!"https://leanprover-community.github.io/mathlib4_docs/find/?pattern={n}"
-                       }]
-  definition d _ := #[{ shortDescription := "def"
-                       , description := s!"Mathlib docs for {d}"
-                       , href := s!"https://leanprover-community.github.io/mathlib4_docs/find/?pattern={d}"
-                       }]
+  const _ _      := #[]
+  definition _ _ := #[]
+
 
 -- def linkTargets : Code.LinkTargets TraverseContext where
---   const n _      := #[ { shortDescription := "doc"
---                        , description := s!"Documentation for {n}"
---                        , href := s!"http://site.example/constlink/{n}"
---                        }
---                      ]
---   definition d _ := #[ { shortDescription := "def"
---                        , description := "Definition"
---                        , href := s!"http://site.example/deflink/{d}"
---                        }
---                      ]
+--   const n _      := #[{ shortDescription := "doc"
+--                        , description := s!"Mathlib docs for {n}"
+--                        , href := s!"https://leanprover-community.github.io/mathlib4_docs/find/?pattern={n}"
+--                        }]
+--   definition d _ := #[{ shortDescription := "def"
+--                        , description := s!"Mathlib docs for {d}"
+--                        , href := s!"https://leanprover-community.github.io/mathlib4_docs/find/?pattern={d}"
+--                        }]
 
 def myblog : Site := site ProofSketches.FrontPage /
   static "assets" ← "assets"
